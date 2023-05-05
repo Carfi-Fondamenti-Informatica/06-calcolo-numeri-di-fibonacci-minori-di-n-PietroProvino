7d29c3e82c55852164abb4b374e56cb2503f0b1a
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-05-05 13:48:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
-    int n, a=1,b=1,c ;
-    do{
-        cin >> n;
-    }while (n<2);
-        for( int j=2; j<=n; j++){
-            cout << a << endl;
-            cout << b << endl;
-            for( int i=2;i<j;i++){
-                c=a+b;
-                a=b;
-                b=c;
-                cout << c << endl;
-            }
-            a=1;
-            b=1;
+    int a=0,b=1,n=0;
+    cin>>n;
+    if(n>=1){
+        cout<<b<<endl;
+        while(true){
+            if((a+b)<=n){
+                cout<<a+b<<endl;
+                if(a<b){a+=b;}
+                else{b+=a;}
+            }else{break;}  	
         }
+    }
     return 0;
 }
